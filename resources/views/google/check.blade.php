a924d5bfe72f6f82deaeda8ae785530aa906a687
EC2 Default User <ec2-user@ip-172-31-38-233.us-east-2.compute.internal>
2020-06-19 03:56:47 +0000

    mm

---
+++ resources/views/google/check.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>公式ページへ移動中</title>
+        <link href="{{ secure_asset('css/check.css') }}" rel="stylesheet">
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    </head>
+    <body>
+        <br>
+        <br>
+        <p>公式ページへ移動しています。<br>詳細は公式ページをご確認下さい。</p>
+        <div class="button">
+                <a href="https://townwork.net/" class="btn-square">公式ページで詳細をみる <i class="fas fa-external-link-alt"></i></a>
+        </div>
+    </body>
+</html>
