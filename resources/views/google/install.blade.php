3087cb472edaed7ff2339584a70fdbcc683b9870
EC2 Default User <ec2-user@ip-172-31-38-233.us-east-2.compute.internal>
2020-06-24 07:20:33 +0000

    mm

--- resources/views/google/install.blade.php
+++ resources/views/google/install.blade.php
@@ -8,24 +8,6 @@
         <link href="{{ secure_asset('css/install.css') }}" rel="stylesheet">
         <!--自動遷移設定-->
         <meta http-equiv="refresh" content=" 1; url=https://a-trade.jp/redirect/moa?media=G16531">
-        <!-- Facebook Pixel Code -->
-        <script>
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '2857819950994297');
-        fbq('track', 'PageView');
-        fbq('track', 'ViewContent');
-        </script>
-        <noscript><img height="1" width="1" style="display:none"
-        src="https://www.facebook.com/tr?id=2857819950994297&ev=PageView&noscript=1"
-        /></noscript>
-        <!-- End Facebook Pixel Code -->
     </head>
     <body>
         <br>
