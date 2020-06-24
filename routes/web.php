dcc954fad3dcda6508af6585d8bcadc3711cfb59
EC2 Default User <ec2-user@ip-172-31-38-233.us-east-2.compute.internal>
2020-06-24 18:42:37 +0000

    Mm

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,8 @@
 */
 
 Route::get('jobs', function () {
-    // return view('google.check'); //仮LP
-    return view('google.jobs'); //獲得LP
+    return view('google.check'); //仮LP
+    // return view('google.jobs'); //獲得LP
 });
 
 //イククル-DL
