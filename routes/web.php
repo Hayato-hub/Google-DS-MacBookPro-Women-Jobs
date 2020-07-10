45529d69fc117a43ae1e6b9e281f3ed6e46e098c
EC2 Default User <ec2-user@ip-172-31-38-233.us-east-2.compute.internal>
2020-07-10 00:27:35 +0000

    mm

--- routes/web.php
+++ routes/web.php
@@ -11,29 +11,13 @@
 |
 */
 
-Route::get('jobs', function () {
-    return view('google.check'); //仮LP
-    // return view('google.rank'); //新：獲得LP
-    // return view('google.jobs'); //旧：獲得LP
+// LP獲得用
+Route::get('matome', function () {
+    return view('google.check'); //確認用LP
+    // return view('google.uraLive'); //獲得LP(1)
 });
-
-//イククル-DL
-Route::get('dl', function () {
-    return view('google.dl'); //自動遷移LP。ココに案件タグを設置
-});
-
-//PoPo-DL　／　旧：モア-DL
-Route::get('install', function () {
-    return view('google.install'); //自動遷移LP。ココに案件タグを設置
-});
-
-// //ライブでゴーゴー-web登録
-// Route::get('register', function () {
-//     return view('google.register'); //自動遷移LP。ココに案件タグを設置
-// });
 
 // LP確認用
 Route::get('cccheck', function () {
-    // return view('google.jobs'); //旧：獲得LP
-    return view('google.rank'); //新：獲得LP
+    return view('google.uraLive'); //獲得LP(1)
 });
