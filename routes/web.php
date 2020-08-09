92b2980368550925252df46117333c876e1ea2e7
EC2 Default User <ec2-user@ip-172-31-38-233.us-east-2.compute.internal>
2020-08-09 11:57:15 +0000

    Mm

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@
 
 // LP獲得用
 Route::get('app', function () {
-    return view('google.check'); //確認用LP
-    // return redirect('https://a-trade.jp/redirect/meruora?media=G16531'); //獲得リダイレクト(3)
+    // return view('google.check'); //確認用LP
+    return redirect('https://a-trade.jp/redirect/meruora?media=G16531'); //獲得リダイレクト(3)
     
     // return view('google.job'); //獲得LP(2)
     // return view('google.uraLive'); //獲得LP(1)
