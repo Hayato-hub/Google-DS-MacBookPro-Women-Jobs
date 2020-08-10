d9b25074017e07cec83ea39b2372423dd69d4f67
EC2 Default User <ec2-user@ip-172-31-38-233.us-east-2.compute.internal>
2020-08-10 00:09:21 +0000

    mm

---
+++ resources/views/google/game.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>テスト</title>
+        <link href="{{ secure_asset('css/game.css') }}" rel="stylesheet">
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    </head>
+    <body>
+        
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,18 @@
 // LP獲得用
 Route::get('app', function () {
     return view('google.check'); //確認用LP
-    // return redirect('https://a-trade.jp/redirect/meruora?media=G16531'); //獲得リダイレクト(3)
+    // return view('google.game'); //獲得LP(3)
     
+    // return redirect('https://a-trade.jp/redirect/meruora?media=G16531'); //獲得リダイレクト(3)
     // return view('google.job'); //獲得LP(2)
     // return view('google.uraLive'); //獲得LP(1)
 });
 
 // LP確認用
 Route::get('cccheck', function () {
-    return redirect('https://a-trade.jp/redirect/meruora?media=G16531'); //獲得リダイレクト(3)
+    return view('google.game'); //獲得LP(3)
     
+    // return redirect('https://a-trade.jp/redirect/meruora?media=G16531'); //獲得リダイレクト(3)
     // return view('google.job'); //獲得LP(2)
     // return view('google.uraLive'); //獲得LP(1)
 });
